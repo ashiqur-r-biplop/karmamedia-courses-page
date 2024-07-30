708e99146108fecfa6d0dd22af9f3192380b4d56
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-31 00:26:05 +0600

    added some ocde

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// 
+// focus input
+const checkbox = document.getElementById("courses-checkbox");
+const coursesList = document.getElementById("courses-list");
+const upIcon = document.querySelector(".fa-caret-up");
+const downIcon = document.querySelector(".fa-caret-down");
+
+checkbox.addEventListener("change", function () {
+  if (checkbox.checked) {
+    console.log(checkbox.checked);
+    upIcon.style.display = "block";
+    downIcon.style.display = "none";
+    coursesList.style.display = "none";
+  } else {
+    upIcon.style.display = "none";
+    downIcon.style.display = "block";
+    coursesList.style.display = "block";
+  }
+});
+
+if (checkbox.checked) {
+  coursesList.style.display = "block";
+
+  upIcon.style.display = "block";
+  downIcon.style.display = "none";
+} else {
+  upIcon.style.display = "none";
+  downIcon.style.display = "block";
+  downIcon.style.display = "block";
+}
