8ce9b442e30181054671bc67fb49ebdab9fcd14a
ashiqur-r-biplop <biplops22@gmail.com>
2024-07-31 16:31:21 +0600

    added some code

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const checkbox = document.getElementById("courses-checkbox");
-  const coursesList = document.getElementById("courses-list");
-  const upIcon = document.querySelector(".fa-caret-up");
-  const downIcon = document.querySelector(".fa-caret-down");
+const checkbox = document.getElementById("courses-checkbox");
+const coursesList = document.getElementById("courses-list");
+const upIcon = document.querySelector(".fa-caret-up");
+const downIcon = document.querySelector(".fa-caret-down");
 
-  checkbox.addEventListener("change", function () {
-    if (checkbox.checked) {
-      console.log(checkbox.checked);
-      upIcon.style.display = "none";
-      downIcon.style.display = "block";
-      coursesList.style.display = "none";
-    } else {
-      upIcon.style.display = "block";
-      downIcon.style.display = "none";
-      coursesList.style.display = "block";
-    }
-  });
-
+checkbox.addEventListener("change", function () {
   if (checkbox.checked) {
-    coursesList.style.display = "block";
+    console.log(checkbox.checked);
     upIcon.style.display = "none";
     downIcon.style.display = "block";
+    coursesList.style.display = "none";
   } else {
     upIcon.style.display = "block";
     downIcon.style.display = "none";
     coursesList.style.display = "block";
   }
 });
+
+if (checkbox.checked) {
+  coursesList.style.display = "block";
+  upIcon.style.display = "none";
+  downIcon.style.display = "block";
+} else {
+  upIcon.style.display = "block";
+  downIcon.style.display = "none";
+  coursesList.style.display = "block";
+}
 const videoLink = {
   course: {
     introduction:
